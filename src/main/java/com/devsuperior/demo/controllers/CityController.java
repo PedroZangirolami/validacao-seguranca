3d2d1d9a780c232d8658d3e79b3a30aade9f8451
PedroZangirolami <pedrozangirolami@gmail.com>
2023-12-01 16:42:54 -0300

    implementando segurança no projeto

--- src/main/java/com/devsuperior/demo/controllers/CityController.java
+++ src/main/java/com/devsuperior/demo/controllers/CityController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -27,6 +28,7 @@ public class CityController {
         return ResponseEntity.ok().body(list);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping()
     public ResponseEntity<CityDTO> insert(@Valid  @RequestBody CityDTO dto){
         dto = service.insert(dto);
